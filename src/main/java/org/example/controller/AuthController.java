a39682f958c00d6924431537d3c17f9d07112db5
hasa10 <rasitha8000@gmail.com>
2025-05-04 19:40:54 +0530

    created AuthController

---
+++ src/main/java/org/example/controller/AuthController.java
@@ -0,0 +1,65 @@
+package org.example.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.dto.AuthenticationRequest;
+import org.example.entity.UserEntity;
+import org.example.repository.UserDao;
+import org.example.util.JwtUtil;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Optional;
+
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final UserDao userDao;
+    private final JwtUtil jwtUtil;
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    @PostMapping("/authenticate")
+    public void createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest,
+                                          HttpServletResponse response) throws IOException, JSONException {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Incorrect username or password.");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        Optional<UserEntity> optionalUser = userDao.findFirstByEmail(userDetails.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        if (optionalUser.isPresent()) {
+            response.getWriter().write(new JSONObject()
+                    .put("userId", optionalUser.get().getId())
+                    .put("role", optionalUser.get().getRole())
+                    .toString()
+            );
+
+            response.addHeader(HEADER_STRING, TOKEN_PREFIX + jwt);
+        }
+    }
+
+}
--- src/main/java/org/example/service/jwt/UserDetailsServiceImpl.java
+++ src/main/java/org/example/service/jwt/UserDetailsServiceImpl.java
@@ -1,12 +1,33 @@
 package org.example.service.jwt;
 
+import lombok.RequiredArgsConstructor;
+import org.example.entity.UserEntity;
+import org.example.repository.UserDao;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserDao userDao;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<UserEntity> optionalUser = userDao.findFirstByEmail(username);
+        if (optionalUser.isEmpty())
+            throw new UsernameNotFoundException("Username not found", null);
+
+        return new org.springframework.security.core.userdetails.User(
+                optionalUser.get().getEmail(),
+                optionalUser.get().getPassword(),
+                new ArrayList<>()
+        );
     }
 }
