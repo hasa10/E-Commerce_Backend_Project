95e7ddfb92f0f5e34cb0885318727edd36880da9
hasa10 <rasitha8000@gmail.com>
2025-05-05 12:25:50 +0530

    creatd createUser method

--- src/main/java/org/example/service/jwt/auth/AuthServiceImpl.java
+++ src/main/java/org/example/service/jwt/auth/AuthServiceImpl.java
@@ -1,6 +1,10 @@
 package org.example.service.jwt.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.SignupRequest;
+import org.example.dto.User;
+import org.example.entity.UserEntity;
+import org.example.enums.UserRole;
 import org.example.repository.UserDao;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,4 +15,24 @@ public class AuthServiceImpl implements AuthService{
     private UserDao userDao;
 
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User createUser(SignupRequest signupRequest) {
+        UserEntity user = new UserEntity();
+
+        user.setEmail(signupRequest.getEmail());
+        user.setName(signupRequest.getName());
+        user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
+        user.setRole(UserRole.CUSTOMER);
+
+        UserEntity createdUser = userDao.save(user);
+
+        User userDto = new User();
+        userDto.setId(createdUser.getId());
+        userDto.setName(createdUser.getName());
+        userDto.setEmail(createdUser.getEmail());
+        userDto.setUserRole(createdUser.getRole());
+
+
+        return userDto;
+    }
 }
