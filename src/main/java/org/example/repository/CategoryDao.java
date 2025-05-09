c1b9cbf22fc7bfd671b0437b78ab86fadbe890ed
hasa10 <rasitha8000@gmail.com>
2025-05-09 15:42:01 +0530

    created CategoryDao

---
+++ src/main/java/org/example/repository/CategoryDao.java
@@ -0,0 +1,10 @@
+package org.example.repository;
+
+import org.example.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryDao extends JpaRepository<CategoryEntity, Long> {
+
+}
