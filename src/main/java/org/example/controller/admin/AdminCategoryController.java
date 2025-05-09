d2f73cb3c60646c4022ae35fc4c0b062fa100f46
hasa10 <rasitha8000@gmail.com>
2025-05-09 16:10:09 +0530

    created createCategory method

--- src/main/java/org/example/controller/admin/AdminCategoryController.java
+++ src/main/java/org/example/controller/admin/AdminCategoryController.java
@@ -2,6 +2,13 @@ package org.example.controller.admin;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.Category;
+import org.example.entity.CategoryEntity;
+import org.example.service.admin.category.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("category")
+    public ResponseEntity<CategoryEntity> createCategory(@RequestBody Category category){
+        CategoryEntity categoryEntity = categoryService.createCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryEntity);
+    }
 }
