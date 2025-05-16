063866a676e06fb0e14c55c5b611728dcbe2f2ff
hasa10 <rasitha8000@gmail.com>
2025-05-16 11:52:04 +0530

    created getAllProducts method in AdminProductController

--- src/main/java/org/example/controller/admin/AdminProductController.java
+++ src/main/java/org/example/controller/admin/AdminProductController.java
@@ -5,12 +5,10 @@ import org.example.dto.Product;
 import org.example.service.admin.adminProduct.AdminProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -23,5 +21,11 @@ public class AdminProductController {
     public ResponseEntity<Product> addProduct(@ModelAttribute Product product) throws IOException {
         Product productAdd = adminProductService.addProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(productAdd);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = adminProductService.getAllProducts();
+        return ResponseEntity.ok(products);
     }
 }
--- src/main/java/org/example/service/admin/adminProduct/AdminProductService.java
+++ src/main/java/org/example/service/admin/adminProduct/AdminProductService.java
@@ -3,8 +3,11 @@ package org.example.service.admin.adminProduct;
 import org.example.dto.Product;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AdminProductService {
 
     Product addProduct(Product product) throws IOException;
+
+    List<Product> getAllProducts();
 }
