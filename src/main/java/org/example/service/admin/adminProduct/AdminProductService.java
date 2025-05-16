2c4bdb262aec3c39ddf7b20b39858467a26f0df7
hasa10 <rasitha8000@gmail.com>
2025-05-16 11:45:16 +0530

    created AdminProductService

--- src/main/java/org/example/service/admin/adminProduct/AdminProductService.java
+++ src/main/java/org/example/service/admin/adminProduct/AdminProductService.java
@@ -1,4 +1,10 @@
 package org.example.service.admin.adminProduct;
 
+import org.example.dto.Product;
+
+import java.io.IOException;
+
 public interface AdminProductService {
+
+    Product addProduct(Product product) throws IOException;
 }
