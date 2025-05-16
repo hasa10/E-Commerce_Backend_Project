1b13ddd08361661035be5eeb142d708828908a4c
hasa10 <rasitha8000@gmail.com>
2025-05-16 11:48:46 +0530

    created addProduct method in controller

--- src/main/java/org/example/controller/admin/AdminProductController.java
+++ src/main/java/org/example/controller/admin/AdminProductController.java
@@ -1,9 +1,16 @@
 package org.example.controller.admin;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.Product;
 import org.example.service.admin.adminProduct.AdminProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -12,4 +19,9 @@ public class AdminProductController {
 
     private final AdminProductService adminProductService;
 
+    @PostMapping("/product")
+    public ResponseEntity<Product> addProduct(@ModelAttribute Product product) throws IOException {
+        Product productAdd = adminProductService.addProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productAdd);
+    }
 }
