51b226fb481c54eea7e4265c59b343632dea1e06
hasa10 <rasitha8000@gmail.com>
2025-05-17 16:43:39 +0530

    creatd OrderEntity

---
+++ src/main/java/org/example/entity/OrderEntity.java
@@ -0,0 +1,42 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.example.enums.OrderStatus;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderDescription;
+
+    private Date date;
+
+    private Long amount;
+
+    private String address;
+
+    private String payment;
+
+    private OrderStatus orderStatus;
+
+    private Long totalAmount;
+
+    private Long discount;
+
+    private UUID trackingId;
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity userEntity;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "orderEntity")
+    private List<CartItemsEntity> cartItems;
+}
