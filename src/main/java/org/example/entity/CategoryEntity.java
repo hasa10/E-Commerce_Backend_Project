71deddb4d8e23fb8f08cb7620404f681e4caf503
hasa10 <rasitha8000@gmail.com>
2025-05-17 15:35:08 +0530

    Updated CategoryEntity

--- src/main/java/org/example/entity/CategoryEntity.java
+++ src/main/java/org/example/entity/CategoryEntity.java
@@ -1,13 +1,16 @@
 package org.example.entity;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "category")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryEntity {
 
     @Id
