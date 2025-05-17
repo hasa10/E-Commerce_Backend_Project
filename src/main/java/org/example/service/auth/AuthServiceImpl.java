2395111123eeccdc1b21ce1c2d8ee5225bb17c58
hasa10 <rasitha8000@gmail.com>
2025-05-17 16:52:53 +0530

    Updated AuthServiceImpl

--- src/main/java/org/example/service/auth/AuthServiceImpl.java
+++ src/main/java/org/example/service/auth/AuthServiceImpl.java
@@ -4,8 +4,11 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.example.dto.SignupRequest;
 import org.example.dto.User;
+import org.example.entity.OrderEntity;
 import org.example.entity.UserEntity;
+import org.example.enums.OrderStatus;
 import org.example.enums.UserRole;
+import org.example.repository.OrderDao;
 import org.example.repository.UserDao;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,8 @@ public class AuthServiceImpl implements AuthService{
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    private final OrderDao orderDao;
+
     public User createUser(SignupRequest signupRequest) {
         UserEntity user = new UserEntity();
 
@@ -25,8 +30,15 @@ public class AuthServiceImpl implements AuthService{
         user.setName(signupRequest.getName());
         user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
         user.setRole(UserRole.CUSTOMER);
-
         UserEntity createdUser = userDao.save(user);
+
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setAmount(0L);
+        orderEntity.setTotalAmount(0L);
+        orderEntity.setDiscount(0L);
+        orderEntity.setUserEntity(createdUser);
+        orderEntity.setOrderStatus(OrderStatus.Pending);
+        orderDao.save(orderEntity);
 
         User userDto = new User();
         userDto.setId(createdUser.getId());
