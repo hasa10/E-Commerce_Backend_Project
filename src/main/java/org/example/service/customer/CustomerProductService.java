2530ad424a1be76c996981ce8969f01e96203e2e
hasa10 <rasitha8000@gmail.com>
2025-05-18 07:41:52 +0530

    Created CustomerProductService

--- src/main/java/org/example/service/customer/CustomerProductService.java
+++ src/main/java/org/example/service/customer/CustomerProductService.java
@@ -1,4 +1,10 @@
 package org.example.service.customer;
 
+import org.example.dto.Product;
+
+import java.util.List;
+
 public interface CustomerProductService {
+    List<Product> getAllProducts();
+    List<Product> searchProductByName(String name);
 }
